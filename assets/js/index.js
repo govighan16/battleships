53a56e1ba6e751f5308c5272342c01980bd2fb93
govighan16 <govighan@gmail.com>
2023-08-03 21:27:06 +0100

    Add game over functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,43 +8,50 @@ console.log(board.grid);
 
 // Your code here
 
+//Handler for clicking cells
+const handleCellClick = event => {
+    const cellElement = event.target;
+    const value = board.makeHit(event.target.dataset.row, event.target.dataset.col);
+
+    if (value === null) {
+        cellElement.classList.add("miss");
+    } else {
+        cellElement.classList.add("hit");
+        cellElement.innerText = value;
+    }
+
+    if (board.isGameOver()) {
+        alert('You won!');
+        const cells = document.querySelectorAll('.cell');
+        cells.forEach(cell => {
+            cell.removeEventListener('click', handleCellClick);
+        });
+        event.stopPropagation();
+    }
+};
 //Visualisation of the board
-    const boardElement = document.createElement("div");
-    boardElement.classList.add("board");
+const boardElement = document.createElement("div");
+boardElement.classList.add("board");
+  
+for (let i = 0; i < board.grid.length; i++) {
+    const rowElement = document.createElement("div");
+    rowElement.classList.add("row");
+  
+for (let j = 0; j < board.grid[i].length; j++) {
+    const cellElement = document.createElement("div");
+    cellElement.classList.add("cell");
+    cellElement.setAttribute("data-row", i);
+    cellElement.setAttribute("data-col", j);
 
-    const handleCellClick = event => {
-        const cellElement = event.target
-        const value = board.makeHit(event.target.dataset.row, event.target.dataset.col);
-      
-        if (value === null) {
-          
-          cellElement.classList.add("miss");
-        } else {
-          
-          cellElement.classList.add("hit");
-          cellElement.innerText = value;
-        }
+    cellElement.addEventListener("click", handleCellClick);
+  
+    rowElement.appendChild(cellElement);
     }
   
-    for (let i = 0; i < board.grid.length; i++) {
-      const rowElement = document.createElement("div");
-      rowElement.classList.add("row");
+    boardElement.appendChild(rowElement);
+}
   
-      for (let j = 0; j < board.grid[i].length; j++) {
-        const cellElement = document.createElement("div");
-        cellElement.classList.add("cell");
-        cellElement.setAttribute("data-row", i);
-        cellElement.setAttribute("data-col", j);
-
-        cellElement.addEventListener("click", handleCellClick);
-  
-        rowElement.appendChild(cellElement);
-      }
-  
-      boardElement.appendChild(rowElement);
-    }
-  
-    document.body.appendChild(boardElement);
+document.body.appendChild(boardElement);
 
       
 
