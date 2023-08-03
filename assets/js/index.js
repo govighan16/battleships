e9e2e18738fb4c98023bdb6651f004db96564f01
govighan16 <govighan@gmail.com>
2023-08-03 20:28:17 +0100

    Add css to visualise grid

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,3 +7,27 @@ let board = new Board(); // creates a new game board
 console.log(board.grid);
 
 // Your code here
+function generateBoardUI(grid) {
+    const boardElement = document.createElement("div");
+    boardElement.classList.add("board");
+  
+    for (let i = 0; i < grid.length; i++) {
+      const rowElement = document.createElement("div");
+      rowElement.classList.add("row");
+  
+      for (let j = 0; j < grid[i].length; j++) {
+        const cellElement = document.createElement("div");
+        cellElement.classList.add("cell");
+        cellElement.setAttribute("data-row", i);
+        cellElement.setAttribute("data-col", j);
+  
+        rowElement.appendChild(cellElement);
+      }
+  
+      boardElement.appendChild(rowElement);
+    }
+  
+    document.body.appendChild(boardElement);
+  }
+
+  generateBoardUI(board.grid)
